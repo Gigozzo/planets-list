f6fcf19c11bab974524a6ebe309ea5b2374cb4fb
Gregory <gigozzo4@gmail.com>
2015-04-08 12:27:28 +0300

    Admin tools

--- planetslist/planets/models.py
+++ planetslist/planets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Planet(models.Model):
+    title = models.CharField(max_length=255)
+    diameter = models.CharField(max_length=255)
+    surfacewater = models.CharField(max_length=255)
+    population = models.CharField(max_length=255)
+    gravity = models.CharField(max_length=255)
+    climate = models.CharField(max_length=255)
+    orbitalperiod = models.CharField(max_length=255)
+    terrain = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/planet/%i/" % self.id
--- planetslist/planetslist/settings_local.py
+++ planetslist/planetslist/settings_local.py
@@ -14,7 +14,7 @@ DATABASES = {
 }
 
 INSTALLED_APPS = (
-	# 'django.contrib.admin',
+	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
