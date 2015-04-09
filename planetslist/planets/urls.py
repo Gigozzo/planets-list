3e82f45eb58c1d51be4c7fcaf23ab27edbf39039
Gregory <gigozzo4@gmail.com>
2015-04-09 11:51:14 +0300

    Fix top bar and urls

--- planetslist/planets/urls.py
+++ planetslist/planets/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import patterns, url
 from planets.views import PlanetsListView, PlanetDetailView
 
 urlpatterns = patterns('',
-	url(r'^$', PlanetsListView.as_view(), name='list'), # то есть по URL http://имя_сайта/planets/
-	url(r'^(?P<pk>\d+)/$', PlanetDetailView.as_view()), # а по URL http://имя_сайта/planets/число/
+	url(r'^$', PlanetsListView.as_view(), name='list'), # то есть по URL http://имя_сайта/
 )
